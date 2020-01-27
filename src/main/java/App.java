95da24cffce416bd22e5ce88d0deee62fe96174d
MichaelOdera <michogelira@gmail.com>
2020-01-27 03:15:26 +0300

    add the source file src/

---
+++ src/main/java/App.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App{
+    public static void main(String[] args) {
+        boolean programRunning = true;
+
+        while(programRunning){
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Welcome to the Ceaser Cipher Application");
+            System.out.println("Enter '1' to Encrypt or '2' to decrypt");
+
+            try {
+                String choice = bufferedReader.readLine();
+                //Function to encrypt
+                if(choice.equals("1")){
+                    Ceaser ceaser = new Ceaser();
+                    System.out.println("Enter the statement that you would like to encrypt");
+                    String textToBeEncrypted = bufferedReader.readLine();
+                    System.out.println("Enter the key the you wish to encrypt with: (between 1 to 25)");
+                    int userKeyToEncrypt = Integer.parseInt(bufferedReader.readLine());
+                    ceaser.ceaserEncryptor(textToBeEncrypted, userKeyToEncrypt);
+
+
+                    System.out.println("Here are your results:");
+                    System.out.println("Key: "+ ceaser.getShiftBy());
+                    System.out.println("Your Statement: "+ ceaser.getSubmittedStatement());
+                    System.out.println("Encryption: " + ceaser.ceaserEncryptor(textToBeEncrypted, userKeyToEncrypt));
+
+
+                //Function to decrypt
+                }else if(choice.equals("2")){
+                    Ceaser ceaser = new Ceaser();
+                    System.out.println("Enter the statement that you would like to be decrypted");
+                    String textToBeDecrypted = bufferedReader.readLine();
+                    System.out.println("Enter the key it was decrypted with: (between 1 to 25)");
+                    int userKeyToDecrypt = Integer.parseInt(bufferedReader.readLine());
+                    ceaser.ceaserEncryptor(textToBeDecrypted, -userKeyToDecrypt);
+
+                    System.out.println("Here are your results:");
+                    System.out.println("DecryptionKey: "+ ceaser.getShiftBy());
+                    System.out.println("Your Statement: "+ ceaser.getSubmittedStatement());
+                    System.out.println("Decryption: " + ceaser.ceaserEncryptor(textToBeDecrypted, -userKeyToDecrypt));
+
+                 //Check if the input entered is part of what is required
+                }else{
+                    System.out.println("You have entered an invalid choice");
+                    programRunning = false;
+                }
+
+
+             //Check if there is an error and reply accordingly
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/test/java/CeaserTest.java
@@ -0,0 +1,82 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+
+
+public class CeaserTest {
+
+    //create  test for an empty statement
+    @Test
+    public void doNotEncrypt_EmptyString(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals(" ", ceaser.ceaserEncryptor(" ", 12));
+
+    }
+
+    //Create test for symbols not to be changed
+    @Test
+    public void doNotEncrypt_Symbols(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals("-", ceaser.ceaserEncryptor("-", 21));
+        assertEquals("!", ceaser.ceaserEncryptor("!", 17));
+        assertEquals("&", ceaser.ceaserEncryptor("&", 20));
+        assertEquals(".", ceaser.ceaserEncryptor(".", 24));
+        String remainingSymbols = "@$^*()#%";
+        assertEquals(remainingSymbols, ceaser.ceaserEncryptor(remainingSymbols, 4));
+    }
+
+
+    //create a test to check for the lower case letters
+    @Test
+    public void encryptSingleLowerCaseLetters(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals("b", ceaser.ceaserEncryptor("b", 0));
+        assertEquals("b", ceaser.ceaserEncryptor("a", 1));
+        assertEquals("k", ceaser.ceaserEncryptor("i", 2));
+    }
+
+    //create test for the uppercase letters
+    @Test
+    public void encryptSingleUpperCaseLetters(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals("X", ceaser.ceaserEncryptor("U", 3));
+        //Check for the end of the cycle at Z
+        assertEquals("C", ceaser.ceaserEncryptor("Z", 3));
+        assertEquals("R", ceaser.ceaserEncryptor("M", 5));
+    }
+
+    //Check for the encryption of whole statements
+    @Test
+    public void encryptWholeLowerCaseWords(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals("eager", ceaser.ceaserEncryptor("eager", 0));
+        //Check for all the letters in the english alphabet in lower case
+        String lowerCaseAllLetters = "the quick brown fox jumped over the lazy dogs";
+        assertEquals(lowerCaseAllLetters, ceaser.ceaserEncryptor(lowerCaseAllLetters, 0));
+        //Check for the shift by 23
+        assertEquals("qeb nrfzh yoltk clu grjmba lsbo qeb ixwv aldp", ceaser.ceaserEncryptor(lowerCaseAllLetters, 23));
+    }
+
+    //Check for upper case whole sentences
+    @Test
+    public void encryptWholeUpperCaseWords(){
+        Ceaser ceaser = new Ceaser();
+        assertEquals("BEAVER", ceaser.ceaserEncryptor("BEAVER", 0));
+        //Check for all the uppercase letters
+        String upperCaseAllLetters = "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOGS";
+        assertEquals("QEB NRFZH YOLTK CLU GRJMBA LSBO QEB IXWV ALDP", ceaser.ceaserEncryptor(upperCaseAllLetters, 23));
+
+    }
+
+    //allow for decryption
+    @Test
+    public  void decryptEnteredEncryptedWords(){
+        Ceaser ceaser = new Ceaser();
+        String reverseStatementSample = "reverse";
+        assertEquals("sfwfstf", ceaser.ceaserEncryptor(reverseStatementSample, 1));
+        assertEquals(reverseStatementSample, ceaser.ceaserEncryptor("sfwfstf", -1));
+
+    }
+
+}
